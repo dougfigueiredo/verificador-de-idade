ecf63eba8e6227dede72e024d7769da871bd7c74
dougfigueiredo <douglas.profi@yahoo.com>
2022-12-11 11:13:38 -0300

    Alterando caminho

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function verificar() {
     let res = document.querySelector('div#res')
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente')
-    } else{
+    } else {
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
         let gênero = ''
@@ -15,32 +15,32 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', '/images/foto-bebe-m.png')
+                img.setAttribute('src', './images/foto-bebe-m.png')
             } else if (idade < 21) {
                 //Jovem
-                img.setAttribute('src', '/images/foto-jovem-m.png')
+                img.setAttribute('src', './images/foto-jovem-m.png')
             } else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', '/images/foto-adulto-m.png')
+                img.setAttribute('src', './images/foto-adulto-m.png')
             } else {
                 //Idoso
-                img.setAttribute('src', '/images/foto-idoso-m.png')
+                img.setAttribute('src', './images/foto-idoso-m.png')
             }
             // window.document.body.style.background = 'green'
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', '/images/foto-bebe-f.png')
+                img.setAttribute('src', './images/foto-bebe-f.png')
             } else if (idade < 21) {
                 //Jovem
-                img.setAttribute('src', '/images/foto-jovem-f.png')
+                img.setAttribute('src', './images/foto-jovem-f.png')
             } else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', '/images/foto-adulto-f.png')
+                img.setAttribute('src', './images/foto-adulto-f.png')
             } else {
                 //Idoso
-                img.setAttribute('src', '/images/foto-idoso-f.png')
+                img.setAttribute('src', './images/foto-idoso-f.png')
             }
             // window.document.body.style.background = 'pink'
         }
